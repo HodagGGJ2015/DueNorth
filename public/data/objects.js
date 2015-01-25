41e9cecc63fc6db33c7245089b2c756c071ca0ef
danilleswank <danielle@danielleswank.com>
2015-01-25 11:06:36 -0800

    added artifact description

--- public/data/objects.js
+++ public/data/objects.js
@@ -9,6 +9,13 @@
         response: 'You picked up the coin.'
       }
     },
+    artifact: {
+      location: 'woods',
+      fullDescription: 'There is an unusual artifact on the ground.'
+      take: {
+        response: 'It was half-buried in the ground. You reach down to pull it out. It’s a weird… artifact of some sort. Whatever it is, I bet your mom would know more about it. You put the artifact in your fanny pack for later.'
+      }
+    },
     wrench: {
       location: 'parking',
       take: {
